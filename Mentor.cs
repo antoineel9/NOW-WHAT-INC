19797563c2230892723040e43a75865e05598043
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2020-01-08 14:24:35 -0600

    Add files via upload

---
+++ Mentor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace new3.Data
+{
+    public class Mentor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string MID { get; set; }
+
+        public string LifeSkills { get; set; }
+        public string ContinuingEd { get; set; }
+        public string YouthCoach { get; set; }
+    }
+}

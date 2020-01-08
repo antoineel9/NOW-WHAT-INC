262317a086647454c361721da9350f9f92fed8c9
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2020-01-08 14:45:37 -0600

    Add files via upload

---
+++ Pages/Services/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using new3.Data;
+
+namespace new3.Pages.Services
+{
+    public class IndexModel : PageModel
+    {
+        private readonly new3.Data.ApplicationDbContext _context;
+
+        public IndexModel(new3.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<GED> GED { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            GED = await _context.GED.ToListAsync();
+        }
+    }
+}
